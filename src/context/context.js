6d4f285b570914f8474690ca3b9b4f118105cddd
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2020-04-03 00:54:42 +0100

    final version with link to contetful data

--- src/context/context.js
+++ src/context/context.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { linkData } from "./linkData";
 import { socialData } from "./socialData";
 import { items } from "./productData";
+import { client } from "./contentful";
 const ProductContext = React.createContext();
 //Provider
 //Consumer
@@ -30,8 +31,13 @@ class ProductProvider extends Component {
   };
   componentDidMount() {
     //from contentful items
-
-    this.setProducts(items);
+    //this.setProducts(items);
+    client
+      .getEntries({
+        content_type: "techStoreProducts"
+      })
+      .then(response => this.setProducts(response.items))
+      .catch(console.error);
   }
 
   //set products
